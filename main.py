b6d356c86a58f036675db339563fc22a31f3948c
Apoquinto <RCLlanesA@gmail.com>
2021-06-16 23:21:07 -0500

    :bookmark: Finish V 1.0

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from src.LinkedList import LinkedList
 from src.hashTable import HashTable
+import random
+
+
+def fakeNames(noNames):
+    result = []
+
+    names = ["Roberto", "Armin", "Pedros", "Leonardo", "Efrain",
+             "Mario", "Apoquinto", "Rodolfo", "Armando", "Dario"]
+    lastnames = ["Llanes", "Zurita", "Tuz", "Buen", "Fin",
+                 "Armando", "Apoquinto", "Torres", "Monsreal", "Peña"]
+
+    for name in range(noNames):
+        result.append(
+            [names[random.randint(0, 9)] + ' ' + names[random.randint(0, 9)] + ' ' +
+             lastnames[random.randint(0, 9)] + ' ' + lastnames[random.randint(0, 9)], name]
+        )
+
+    return result
+
+
+def example1():
+    table = HashTable(15)
+
+    table.add("Roberto Carlos", "Calle 121 Cinco Colonias")
+    table.add("Carlos Roberto", "Calle 121 Cinco Colonias")
+
+    print(table.search("Roberto Carlos"))
+
+
+def example2():
+    table = HashTable(10)
+
+    for key, value in fakeNames(50):
+        table.add(key, value)
+
+    print(table.printTable())
 
 
 def main():
-    table = HashTable(5)
-
-    table.addElement(2, 13)
-    table.addElement(2, 23)
-    table.addElement(2, 33)
-
-    print("Test", table.getBucket(2))
-
-    table.printTable()
+    example2()
 
 
 if __name__ == '__main__':
--- src/hashTable.py
+++ src/hashTable.py
@@ -1,16 +1,37 @@
-from src.LinkedList import LinkedList, Node
-
-
 class HashTable():
     def __init__(self, buckets):
-        self.table = [LinkedList("->") for element in range(buckets)]
+        self.noBuckets = self.getMaxPrime(buckets)
+        self.table = [[] for bucket in range(self.noBuckets)]
 
-    def addElement(self, index, value):
-        return self.table[index].addNode(value)
+    def add(self, key, value):
+        self.insert(self.hashFun(key), value)
+
+    def search(self, key):
+        return self.table[self.hashFun(key)]
+
+    def hashFun(self, key):
+        result = 0
+        for char in str(key):
+            result += ord(char)
+
+        return result % self.noBuckets
+
+    def insert(self, index, value):
+        self.table[index].append(value)
 
     def printTable(self):
-        for bucket in range(len(self.table)):
-            print(self.getBucket(bucket))
+        for bucket in self.table:
+            print(bucket)
+
+    def getMaxPrime(self, n):
+        compuests = set()
+        primes = []
+        for i in range(2, n+1):
+            if i not in compuests:
+                primes.append(i)
+                compuests.update(range(i*i, n+1, i))
+
+        return primes[-1]
 
     def getBucket(self, index):
-        return self.table[index].getList(self.table[index].getInitial())
+        return self.table[index]
